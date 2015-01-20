342ea776d54f905d7294fadcd9fca8926c358da9
Piotr Szymanski <szyman@magres.net>
2015-01-20 09:30:25 +0100

    Add method descriptions

--- src/ObjectAccess/Resource/Addressing/ResourceAddress.php
+++ src/ObjectAccess/Resource/Addressing/ResourceAddress.php
@@ -9,6 +9,7 @@ use Light\ObjectAccess\Query\Scope;
 interface ResourceAddress
 {
 	/**
+	 * Returns a new address with the given scope appended at the end.
 	 * @param Scope $scope
 	 * @return ResourceAddress	A new ResourceAddress object representing the original address
 	 *                          with the scope object appended at the end.
@@ -16,13 +17,22 @@ interface ResourceAddress
 	public function appendScope(Scope $scope);
 
 	/**
+	 * Returns a new address with the given element appended at the end.
 	 * @param string	$pathElement
 	 * @return ResourceAddress	A new ResourceAddress object representing the original address
 	 * 							with the new element appended at the end.
 	 */
 	public function appendElement($pathElement);
 
+	/**
+	 * Returns true if the address has a string representation.
+	 * @return boolean
+	 */
 	public function hasStringForm();
 
+	/**
+	 * Returns the string representation of this address.
+	 * @return string	An address string, if it is available; otherwise, NULL.
+	 */
 	public function getAsString();
 }
